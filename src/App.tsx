0a124c7bedc445d119185014240e0c74b09429e1
Raia Quitoriano <rsquitoriano@gmail.com>
2025-02-17 23:57:10 +0800

    test mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Chat from './components/Chat';
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener('resize', setVh);
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+
   return (
     <div className="app">
       <Header />
